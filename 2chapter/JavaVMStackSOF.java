f0af936878f33c80f7c2c3028d787d7698ce37c5
高曾荣 <zengrong.gzr@alibaba-inc.com>
2017-02-04 20:22:59 +0800

    VM Stack SOF(stack over flow)

---
+++ 2chapter/JavaVMStackSOF.java
@@ -0,0 +1,23 @@
+/**
+ * 虚拟机栈和本地方法栈溢出
+ * VM Args：-Xss128k
+ * Created by gaozengrong on 17/2/4.
+ */
+public class JavaVMStackSOF {
+    private int stackLength = 1;
+
+    public void stackLeak() {
+        stackLength++;
+        stackLeak();
+    }
+
+    public static void main(String[] args) throws Throwable {
+        JavaVMStackSOF oom = new JavaVMStackSOF();
+        try {
+            oom.stackLeak();
+        } catch (Throwable e) {
+            System.out.println("stack length:" + oom.stackLength);
+            throw e;
+        }
+    }
+}
