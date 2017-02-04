2cfb474472bbb0c2d804142231c4c5bcffd1a798
高曾荣 <zengrong.gzr@alibaba-inc.com>
2017-02-04 20:23:42 +0800

    Method Area OOM

---
+++ 2chapter/JavaMethodAreaOOM.java
@@ -0,0 +1,30 @@
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * 借助CGLIB使方法区出现内存溢出异常
+ * VM Args： -XX:PermSize=10M -XX:MaxPermSize=10M
+ * Created by gaozengrong on 17/2/4.
+ */
+public class JavaMethodAreaOOM {
+    public static void main(String[] args) {
+        while (true) {
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(OOMObject.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback(new MethodInterceptor() {
+                public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+                    return proxy.invokeSuper(obj, args);
+                }
+            });
+            enhancer.create();
+        }
+    }
+
+    static class OOMObject {
+
+    }
+}
